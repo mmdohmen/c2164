6e31a3645367ade1527211c10b0e6c225c3a9837
Zeta <83716941+mmdohmen@users.noreply.github.com>
2021-11-15 22:40:04 -0300

    Add files via upload

---
+++ phpActividadU2.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h1> Codo a Codo - PHP </h1>";
+echo "<h3> Comision 2164 </h3>";
+
+echo "<h5> ALUMNO </h5>";
+echo "nombre:   Mario Maximo DOHMEN" . "<br>";
+echo "edad:     54" . "<br>";
+echo "hobbies:  programacion - ciclismo - arqueria - motociclismo - guitarra - defensa" . "<br>";
+echo "editor de codigo preferido:   Visual Studio Code" . "<br>";
+echo "sistema operativo:   Windows 10" . "<br>";
+
+?>
